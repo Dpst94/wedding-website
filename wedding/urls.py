45514119c5fff67acb76aa561c6dba52dcc6b3f7
dpst <dpst.ho@gmail.com>
2017-08-03 00:10:51 +0200

    Added the ‘To Do’ page with links to detail page of the activities.

--- wedding/urls.py
+++ wedding/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^activities/$', views.activities, name='activitites'),
+    url(r'^activity/(?P<pk>\d+)/$', views.activity_detail, name='activity_detail')
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Activity
 
 def homepage(request):
     return render(request, 'wedding/homepage.html', {})
 
 def activities(request):
-    pass
+    activities = Activity.objects.order_by('deadline')
+    return render(request, 'wedding/activities.html', {'activities':activities})
+
+def activity_detail(request, pk):
+    activity = get_object_or_404(Activity, pk=pk)
+    return render(request, 'wedding/activity_detail.html', {'activity': activity})
